1c4affd27b50db4171f37880d35d3286737e2582
yoosooyoung <wndghk202@naver.com>
2021-12-27 18:47:47 +0900

    javascript 211227

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 2;
+
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "nicolas";
+console.log("hello " + myName);
+
+const myName = "nico";
+myName = "nicolas"
+
+console.log("hello " + myName);
