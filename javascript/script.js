9a0947818c27e50d815e78b96b9f03cc32cff139
yoosooyoung <wndghk202@naver.com>
2021-12-31 21:43:49 +0900

    211231 조건문, 함수

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,14 @@
-const age = 20;
-
-//한국나이를 계산하는 함수
-//return -> 결과를 받고 싶다.
-function calculateKrAge(ageOfForeigner) {
-	return ageOfForeigner + 2;
+const age = parseInt(prompt('how old are you?'));
+if (isNaN(age) || age < 0) {
+	//조건 === true
+	console.log('숫자를 확인해주세요.');
+} else if (age < 18) {
+	console.log('너는 어리다.');
+} else if (age >= 18 && age <= 50) {
+	console.log('음주 가능합니다.');
+} else if (age > 50 && age <= 80) {
+	console.log('운동해야합니다.');
+} else {
+	//위의 네 조건이 false일 때
+	console.log('음주를 하면 위험합니다.');
 }
-
-const krAge = calculateKrAge(age); //return값이 온다.
-
-console.log(krAge); //22
