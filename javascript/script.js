9fa465a8a2a50ba7b0986191fb8cd2168d2c25fe
yoosooyoung <wndghk202@naver.com>
2022-01-01 14:41:41 +0900

    220122 element 가져오기

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,11 @@
-const title = document.getElementById('title');
-console.dir(title);
-title.innerText = 'Got you!';
+const hellos = document.getElementsByClassName('hello');
+console.log(hellos); //HTMLCollection(3) [div.hello, div.hello, div.hello]
 
-console.log(title.id);
-console.log(title.className);
+const div = document.getElementsByTagName('h1');
+console.log(div); //HTMLCollection(3) [h1, h1, h1]
+
+const title = document.querySelector('.hello h1');
+console.log(title); //h1
+
+const title2 = document.querySelectorAll('.hello h1');
+console.log(title2); //NodeList(3) [h1, h1, h1]
