0fba29142000511c63e952f195ad87847e1d0b4e
yoosooyoung <wndghk202@naver.com>
2022-01-03 21:40:04 +0900

    220103 css in JavaScript

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,15 @@
-const hellos = document.getElementsByClassName('hello');
-console.log(hellos); //HTMLCollection(3) [div.hello, div.hello, div.hello]
+const title = document.querySelector('.hello:first-child h1');
 
-const div = document.getElementsByTagName('h1');
-console.log(div); //HTMLCollection(3) [h1, h1, h1]
+function handleTitleClick() {
+	const currentColor = title.style.color;
+	let newColor;
+	//파란색이면 토마토색으로, 토마토색이면 파란색으로
+	if (currentColor == 'blue') {
+		newColor = 'tomato';
+	} else {
+		newColor = 'blue';
+	}
+	title.style.color = newColor;
+}
 
-const title = document.querySelector('.hello h1');
-console.log(title); //h1
-
-const title2 = document.querySelectorAll('.hello h1');
-console.log(title2); //NodeList(3) [h1, h1, h1]
+title.addEventListener('click', handleTitleClick);
