b4d71cf3c50f0f48601880319614d41da71868ec
yoosooyoung <wndghk202@naver.com>
2022-01-04 20:22:11 +0900

    220104 css in javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,21 @@
 const title = document.querySelector('.hello:first-child h1');
 
 function handleTitleClick() {
-	const currentColor = title.style.color;
-	let newColor;
-	//파란색이면 토마토색으로, 토마토색이면 파란색으로
-	if (currentColor == 'blue') {
-		newColor = 'tomato';
-	} else {
-		newColor = 'blue';
-	}
-	title.style.color = newColor;
+	//변수를 활용하면 오류를 줄일 수 있다.
+	const clickedClass = 'active';
+
+	//if (title.className === clickedClass) {
+	//if (title.classList.contains(clickedClass)) {
+	//버그 : 기존 class를 대체한다.
+	//title.className = '';
+	//title.classList.remove(clickedClass);
+	//} else {
+	//title.className = clickedClass
+	//title.classList.add(clickedClass);
+	//}
+	title.classList.toggle('active');
 }
 
 title.addEventListener('click', handleTitleClick);
+
+//style은 css, animation은 javascript
