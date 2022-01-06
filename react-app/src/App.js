a13ab919dd9d4a1486d22d40649071f0ee99c894
yoosooyoung <wndghk202@naver.com>
2022-01-06 22:43:38 +0900

    220106 react component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 
+//react가 가진 Component 클래스를 상속받은 App
+//render 메소드를 가지고 있다.
 class App extends Component {
 	render() {
 		return <div classNme="App">Hello, React!!</div>;
 	}
 }
-
 export default App;
+
+//subject라는 이름을 가진 Component를 만들겠다.
+class Subject extends Component {
+	render() {
+		return (
+			<header>
+				<h1>WEB</h1>
+				word wide web
+			</header>
+		);
+	}
+}
