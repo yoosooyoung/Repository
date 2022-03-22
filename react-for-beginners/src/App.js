45ee996abe99744b7f66fe0050056aee3ad91cc8
yoosooyoung <wndghk202@naver.com>
2022-03-22 20:53:22 +0900

    220322 react cru 연습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,39 +1,52 @@
-import { useState, useEffect } from 'react';
-//state가 변하면 랜더된다 -> componet가 처음 실행될때만 코드 실행
+import { useState } from 'react';
+
 function App() {
-	const [counter, setvalue] = useState(0);
-	const [keyword, setKeyword] = useState('');
-	const onClick = () => setvalue((prev) => prev + 1);
-	const onChange = (event) => {
-		setKeyword(event.target.value);
-	};
-	useEffect(() => {
-		console.log('한번만 실행된다.');
-	}, []);
-	//keyword가 체인지되었을때만 변화하고싶은대 counter변화할때도 랜더링됨
-	//console.log('검색', keyword);
-	//keyword가 변화할때만 랜더링된다.
-	//function, dependency
-	useEffect(() => {
-		if (keyword !== '' && keyword.length > 5) {
-			console.log('키워드가 바뀐다.');
+	//To-Do (문자열 초기화)
+	const [toDo, setToDo] = useState('');
+	//To-Do List (배열 초기화)
+	const [toDos, setToDos] = useState([]);
+	//To-Do를 입력하면 입력된 값이 세팅된다.
+	const onChange = (event) => setToDo(event.target.value);
+	//To-Do를 추가하면 입력창은 빈문자열이 되된다.
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === '') {
+			return;
 		}
-	}, [keyword]);
-	useEffect(() => {
-		console.log('카운터가 바뀐다.');
-	}, [counter]);
+		//새로운 배열을 toDo와 이전배열을 가져온다.
+		setToDos((currentArray) => [toDo, ...currentArray]);
+		setToDo('');
+	};
+	const remove = (event) => {
+		const index = event.target.value;
+		toDos.splice(index, index + 1);
+		setToDos([...toDos]);
+	};
 	return (
 		<div>
-			<input
-				onChange={onChange}
-				type="text"
-				placeholder="검색"
-				value={keyword}
-			></input>
-			<h1>{counter}</h1>
-			<button onClick={onClick}>click me</button>
+			<h1>My To Dos ({toDos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={toDo}
+					onChange={onChange}
+					type="text"
+					placeholder="할일을 적으세요"
+				></input>
+				<button>Add To Do</button>
+			</form>
+			<hr />
+			<ul>
+				{toDos.map((item, index) => (
+					//toDos 배열에 있는 item을 가져온다.
+					<li key={index}>
+						{item}
+						<button value={index} onClick={remove}>
+							삭제
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
-
 export default App;
