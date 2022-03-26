d9d782fd8d11d13d3b8d770842255adb33ceacae
yoosooyoung <wndghk202@naver.com>
2022-03-26 10:08:34 +0900

    220325 리액트 무비1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,51 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-	//To-Do (문자열 초기화)
-	const [toDo, setToDo] = useState('');
-	//To-Do List (배열 초기화)
-	const [toDos, setToDos] = useState([]);
-	//To-Do를 입력하면 입력된 값이 세팅된다.
-	const onChange = (event) => setToDo(event.target.value);
-	//To-Do를 추가하면 입력창은 빈문자열이 되된다.
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if (toDo === '') {
-			return;
-		}
-		//새로운 배열을 toDo와 이전배열을 가져온다.
-		setToDos((currentArray) => [toDo, ...currentArray]);
-		setToDo('');
+	const [loading, setLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+	const getMovies = async () => {
+		const response = await fetch(
+			`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+		);
+		const json = await response.json();
+		setMovies(json.data.movies);
+		setLoading(false);
 	};
-	const remove = (event) => {
-		const index = event.target.value;
-		toDos.splice(index, index + 1);
-		setToDos([...toDos]);
-	};
+	useEffect(() => {
+		getMovies();
+	}, []);
+
 	return (
 		<div>
-			<h1>My To Dos ({toDos.length})</h1>
-			<form onSubmit={onSubmit}>
-				<input
-					value={toDo}
-					onChange={onChange}
-					type="text"
-					placeholder="할일을 적으세요"
-				></input>
-				<button>Add To Do</button>
-			</form>
-			<hr />
-			<ul>
-				{toDos.map((item, index) => (
-					//toDos 배열에 있는 item을 가져온다.
-					<li key={index}>
-						{item}
-						<button value={index} onClick={remove}>
-							삭제
-						</button>
-					</li>
-				))}
-			</ul>
+			{loading ? (
+				<h1>Loading....</h1>
+			) : (
+				<div>
+					{movies.map((movie) => (
+						<div key={movie.id}>
+							<img src={movie.medium_cover_image} />
+							<h2>{movie.title}</h2>
+							<p>{movie.summary}</p>
+							<ul>
+								{movie.genres.map((g) => (
+									<li key={g}>{g}</li>
+								))}
+							</ul>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
